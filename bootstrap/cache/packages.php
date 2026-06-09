eae18731fd1ee8251e7b553fc5365cf3c3684de3
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-10 00:08:18 +0500

    all pending

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,1 +1,24 @@
-﻿<?php return [];
+<?php return array (
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
+
