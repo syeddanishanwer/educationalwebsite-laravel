d82975e718fff025a00a20c011658986817d6cff
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 16:47:04 +0500

    Misc changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,15 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Reroute compiled Blade templates to the writable /tmp directory on Vercel
-        if (env('VERCEL_JOB_ID') || env('NOW_REGION')) {
-            $vPath = '/tmp/storage/framework/views';
-            
-            if (!is_dir($vPath)) {
-                mkdir($vPath, 0755, true);
-            }
-            
-            config(['view.compiled' => $vPath]);
+        $vPath = '/tmp/storage/framework/views';
+        
+        if (!is_dir($vPath)) {
+            mkdir($vPath, 0755, true);
         }
+        
+        config(['view.compiled' => $vPath]);
     }
 }
