7bd5188ba50359527d66f9d95adfe98db94f2c9e
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 13:19:54 +0500

    Update SSL verification constant for PHP 8.5 compatibility

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) + [
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT : PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => false,
             ] : [],
         ],
 
