c83371bab59ef4745c25bfef56e42670539371e5
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 15:15:26 +0500

    Corrected API and vercel json file yet again

--- api/index.php
+++ api/index.php
@@ -1,17 +1,2 @@
 <?php
-
-define('LARAVEL_START', microtime(true));
-
-chdir(__DIR__ . '/../');
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
