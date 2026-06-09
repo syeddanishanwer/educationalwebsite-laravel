45b8624e0cf479650c46c18fde5945d2b91f78fa
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 16:12:34 +0500

    Changes to AppServiceProviders.php file

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,34 +1,7 @@
 <?php
 
-namespace App\Providers;
+use App\Providers\AppServiceProvider;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        // Reroute compiled Blade templates to the writable /tmp directory on Vercel
-        if (env('VERCEL_JOB_ID') || env('NOW_REGION')) {
-            $vPath = '/tmp/storage/framework/views';
-            
-            if (!is_dir($vPath)) {
-                mkdir($vPath, 0755, true);
-            }
-            
-            config(['view.compiled' => $vPath]);
-        }
-    }
-}
+return [
+    AppServiceProvider::class,
+];
