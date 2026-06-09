03c0b72a579f6709d2025c6f75bf6247210d5137
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 15:42:12 +0500

    New try on index.php (take 2)

--- api/index.php
+++ api/index.php
@@ -2,15 +2,12 @@
 
 define('LARAVEL_START', microtime(true));
 
-// Critical: set correct working directory for Vercel
 chdir(__DIR__ . '/../');
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Fix paths for serverless
-$app->useBasePath(__DIR__ . '/../');
 $app->usePublicPath(__DIR__ . '/../public');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
