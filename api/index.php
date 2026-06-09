d9a3dd364d4fe21e32d1d0544f752fc1dbd9a4be
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 16:51:59 +0500

    Misc changes

--- api/index.php
+++ api/index.php
@@ -2,6 +2,21 @@
 
 define('LARAVEL_START', microtime(true));
 
+// Create required /tmp directories BEFORE Laravel boots
+$directories = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/storage/app/public',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
