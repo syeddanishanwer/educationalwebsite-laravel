30b9384f8c5956e0ef85c6a949eab036f8edec93
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 15:45:53 +0500

    New try on index.php (take 3)

--- api/index.php
+++ api/index.php
@@ -2,18 +2,8 @@
 
 define('LARAVEL_START', microtime(true));
 
-chdir(__DIR__ . '/../');
-
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->usePublicPath(__DIR__ . '/../public');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Illuminate\Http\Request::capture());
