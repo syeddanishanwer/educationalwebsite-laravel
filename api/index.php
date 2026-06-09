beff6ef2e9a369505e718829c225893169a7c77a
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-06-09 17:34:00 +0500

    Misc Second last

--- api/index.php
+++ api/index.php
@@ -2,8 +2,21 @@
 
 define('LARAVEL_START', microtime(true));
 
+foreach ([
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/storage/app',
+    '/tmp/bootstrap/cache',
+] as $dir) {
+    if (!is_dir($dir)) mkdir($dir, 0775, true);
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
 
 $app->handleRequest(Illuminate\Http\Request::capture());
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,18 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// Create /tmp directories before anything
-foreach ([
-    '/tmp/storage/framework/views',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/logs',
-    '/tmp/storage/app',
-    '/tmp/bootstrap/cache',
-] as $dir) {
-    if (!is_dir($dir)) mkdir($dir, 0775, true);
-}
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -27,6 +15,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })
-    ->create()
-    ->useStoragePath('/tmp/storage');
+    })->create();
